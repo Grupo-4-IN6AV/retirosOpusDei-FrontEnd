84122fe6c4596e3291b4380b8e7d42590ac19df7
rcardenas-2020166 <rcardenas-2020166@kinal.edu.gt>
2022-09-01 21:05:09 -0600

    Purga de Archivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,40 +7,7 @@ import { LoginComponent } from './components/landingPage/login/login.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { NavbarComponent } from './components/landingPage/navbar/navbar.component';
 import { LandingPageComponent } from './components/landingPage/landing-page/landing-page.component';
-import { AboutUsComponent } from './components/landingPage/about-us/about-us.component';
 import { ContactUsComponent } from './components/landingPage/contact-us/contact-us.component';
-import { HotelComponent } from './components/landingPage/hotel/hotel.component';
-import { HotelAdminComponent } from './components/admin/hotel-admin/hotel-admin.component';
-import { EventsAdminComponent } from './components/admin/event-admin/event-admin.component';
-import { HotelViewComponent } from './components/landingPage/hotel-view/hotel-view.component';
-import { ServicesAdminComponent } from './components/admin/services-admin/services-admin.component';
-import { RoomAdminComponent } from './components/admin/room-admin/room-admin.component';
-import { TypeRoomAdminComponent } from './components/admin/type-room-admin/type-room-admin.component';
-
-
-import { LayoutAdminHotelComponent } from './components/hotelAdmin/layout-admin-hotel/layout-admin-hotel.component';
-import { HomeAdminHotelComponent } from './components/hotelAdmin/home-admin-hotel/home-admin-hotel.component';
-import { ProfileAdminHotelComponent } from './components/hotelAdmin/profile-admin-hotel/profile-admin-hotel.component';
-import { ServicesAdminHotelComponent } from './components/hotelAdmin/services-admin-hotel/services-admin-hotel.component';
-import { EventsAdminHotelComponent } from './components/hotelAdmin/events-admin-hotel/events-admin-hotel.component';
-import { TypesRoomsAdminHotelComponent } from './components/hotelAdmin/types-rooms-admin-hotel/types-rooms-admin-hotel.component';
-import { RoomsAdminHotelComponent } from './components/hotelAdmin/rooms-admin-hotel/rooms-admin-hotel.component';
-import { ViewRoomDetailsComponent } from './components/landingPage/view-room-details/view-room-details.component';
-
-import { LayoutUserComponent } from './components/user/layout-user/layout-user.component';
-import { HomeUserComponent } from './components/user/home-user/home-user.component';
-import { HotelUserComponent } from './components/user/hotel-user/hotel-user.component'
-import { HotelAdminHotelComponent } from './components/hotelAdmin/hotel-admin-hotel/hotel-admin-hotel.component';
-
-import { AdminHotelGuard } from './guards/admin-hotel.guard'
-import { AdminGuard } from './guards/admin.guard'
-import { UserGuard } from './guards/user.guard'
-import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
-import { UserCalendarComponent } from './components/user/user-calendar/user-calendar.component';
-import { HistoryComponent } from './components/user/history/history.component';
-import { ReservationsReportComponent } from './components/admin/reservations-report/reservations-report.component';
-import { ReservationsReportComponentHotel } from './components/hotelAdmin/reservations-report/reservations-report.component';
-import { ReservationsComponent } from './components/user/reservations/reservations.component';
 import { DevelopsComponent } from './components/landingPage/develops/develops.component';
 
 const routes: Routes =
@@ -51,10 +18,7 @@ const routes: Routes =
       {path: '', component: LandingPageComponent},
       {path: 'desarrolladores', component: DevelopsComponent},
       {path: 'contactanos', component: ContactUsComponent},
-      {path: 'hotels', component: HotelComponent},
       {path: 'login', component: LoginComponent},
-      {path: 'viewRoom/:id', component: HotelViewComponent},
-      {path: 'viewRoomDetails/:id', component: ViewRoomDetailsComponent},
     ]
   },
   {
@@ -62,35 +26,6 @@ const routes: Routes =
     [
       {path: 'home', component: HomeAdminComponent},
       {path: 'user', component: AdminUserComponent},
-      {path: 'hotel', component: HotelAdminComponent},
-      {path: 'event', component: EventsAdminComponent},
-      {path: 'service', component: ServicesAdminComponent},
-      {path: 'room', component: RoomAdminComponent},
-      {path: 'typeRoom', component: TypeRoomAdminComponent},
-      {path: 'reservations', component: ReservationsReportComponent},
-    ]
-  },
-  {
-    path: 'admin-hotel', canActivate:[AdminHotelGuard], component:LayoutAdminHotelComponent, children:
-    [
-      {path: 'home', component: HomeAdminHotelComponent},
-      {path: 'profile', component: ProfileAdminHotelComponent},
-      {path: 'hotel', component: HotelAdminHotelComponent},
-      {path: 'event', component: EventsAdminHotelComponent},
-      {path: 'service', component: ServicesAdminHotelComponent},
-      {path: 'room', component: RoomsAdminHotelComponent},
-      {path: 'typeRoom', component: TypesRoomsAdminHotelComponent},
-      {path: 'reservations', component: ReservationsReportComponentHotel}
-    ]
-  },
-  {
-    path: 'user', canActivate:[UserGuard], component:LayoutUserComponent, children:
-    [
-      {path: 'home', component: HotelUserComponent},
-      {path: 'profile', component: UserProfileComponent},
-      {path: 'calendar', component: UserCalendarComponent},
-      {path: 'reservations', component: ReservationsComponent},
-      {path: 'history', component: HistoryComponent},
     ]
   },
   { path: '**', component: NotFoundPageComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -62,7 +62,6 @@ import {
 import { ClickOutsideModule } from 'ng-click-outside';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminUserComponent } from './components/admin/admin-user/admin-user.component';
-import { HotelAdminComponent } from './components/admin/hotel-admin/hotel-admin.component';
 import { HomeAdminComponent } from './components/admin/home-admin/home-admin.component';
 import { SidebarAdminComponent } from './components/admin/sidebar-admin/sidebar-admin.component';
 import { SidebarrightAdminComponent } from './components/admin/sidebarright-admin/sidebarright-admin.component';
@@ -72,52 +71,7 @@ import { NotFoundPageComponent } from './components/not-found-page/not-found-pag
 import { LandingPageComponent } from './components/landingPage/landing-page/landing-page.component';
 import { AboutUsComponent } from './components/landingPage/about-us/about-us.component';
 import { ContactUsComponent } from './components/landingPage/contact-us/contact-us.component';
-import { HotelComponent } from './components/landingPage/hotel/hotel.component';
-import { SearchHotelPipe } from './pipes/searchHotelPipe/search-hotel.pipe';
-import { EventsAdminComponent } from './components/admin/event-admin/event-admin.component';
-import { HotelViewComponent } from './components/landingPage/hotel-view/hotel-view.component';
-import { SearchHotelTablePipe } from './pipes/searchHotelTable/search-hotel-table.pipe';
-import { SearchEventPipe } from './pipes/searchEventPipe/search-event.pipe';
-import { ServicesAdminComponent } from './components/admin/services-admin/services-admin.component';
-import { SearchServicePipe } from './pipes/searchService/search-service.pipe';
-import { RoomAdminComponent } from './components/admin/room-admin/room-admin.component';
-import { TypeRoomAdminComponent } from './components/admin/type-room-admin/type-room-admin.component';
-import { SearchTypeRoomPipe } from './pipes/searchTypeRoom/search-type-room.pipe';
-import { SearchRoomPipe } from './pipes/searchRoom/search-room.pipe';
-import {HeaderAdminHotelComponent} from './layout/header-admin-hotel/header-admin-hotel.component'
-import { SidebarAdminHotelComponent } from './components/hotelAdmin/sidebar-admin-hotel/sidebar-admin-hotel.component';
-import { LayoutAdminHotelComponent } from './components/hotelAdmin/layout-admin-hotel/layout-admin-hotel.component';
-import { HomeAdminHotelComponent } from './components/hotelAdmin/home-admin-hotel/home-admin-hotel.component';
-import { SidebarrightAdminHotelComponent } from './components/hotelAdmin/sidebarright-admin-hotel/sidebarright-admin-hotel.component';
-import { ProfileAdminHotelComponent } from './components/hotelAdmin/profile-admin-hotel/profile-admin-hotel.component';
-import { ServicesAdminHotelComponent } from './components/hotelAdmin/services-admin-hotel/services-admin-hotel.component';
-import { EventsAdminHotelComponent } from './components/hotelAdmin/events-admin-hotel/events-admin-hotel.component';
-import { TypesRoomsAdminHotelComponent } from './components/hotelAdmin/types-rooms-admin-hotel/types-rooms-admin-hotel.component';
-import { RoomsAdminHotelComponent } from './components/hotelAdmin/rooms-admin-hotel/rooms-admin-hotel.component';
-import { SearchUserBarPipe } from './pipes/searchUserBar/search-user-bar.pipe';
-import { SearchEventBarPipe } from './pipes/searchEventBar/search-event-bar.pipe';
-import { SearchHotelBarPipe } from './pipes/searchHotelBar/search-hotel-bar.pipe';
-import { SearchRoomBarPipe } from './pipes/searchRoomBar/search-room-bar.pipe';
-import { SearchServiceBarPipe } from './pipes/searchServiceBar/search-service-bar.pipe';
-import { SearchTypeRoomBarPipe } from './pipes/searchTypeRoomBar/search-type-room-bar.pipe';
-import { ViewRoomDetailsComponent } from './components/landingPage/view-room-details/view-room-details.component';
-import { SearchEventHotelPipe } from './pipes/searchEventHotel/search-event-hotel.pipe';
-import { SearchServiceHotelPipe } from './pipes/searchServiceHotel/search-service-hotel.pipe';
-import { SearchRoomHotelPipe } from './pipes/searchRoomHotel/search-room-hotel.pipe';
-import { SearchTypeRoomHotelPipe } from './pipes/searchTypeRoomHotel/search-type-room-hotel.pipe';
-import { SidebarUserComponent } from './components/user/sidebar-user/sidebar-user.component';
-import { SidebarrightUserComponent } from './components/user/sidebarright-user/sidebarright-user.component';
-import { LayoutUserComponent } from './components/user/layout-user/layout-user.component';
-import {HeaderUserComponent} from './layout/header-user/header-user.component';
-import { HomeUserComponent } from './components/user/home-user/home-user.component';
-import { HotelUserComponent } from './components/user/hotel-user/hotel-user.component';
-import { HotelAdminHotelComponent } from './components/hotelAdmin/hotel-admin-hotel/hotel-admin-hotel.component';
-import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
-import { UserCalendarComponent } from './components/user/user-calendar/user-calendar.component';
-import { HistoryComponent } from './components/user/history/history.component';
-import { SearchRoomViewPipe } from './pipes/searchRoomView/search-room-view.pipe';
-import { ReservationsReportComponentHotel } from './components/hotelAdmin/reservations-report/reservations-report.component';
-import { ReservationsComponent } from './components/user/reservations/reservations.component';
+
 import { NavbarComponent } from './components/landingPage/navbar/navbar.component';
 import { DevelopsComponent } from './components/landingPage/develops/develops.component';
 import { SearchUserPipe } from './pipes/adminPipes/searchUser/search-user.pipe';
@@ -137,7 +91,6 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
         RightSidebarComponent,
         HomeAdminComponent,
         AdminUserComponent,
-        HotelAdminComponent,
         SidebarAdminComponent,
         SidebarrightAdminComponent,
         LayoutAdminComponent,
@@ -146,52 +99,6 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
         LandingPageComponent,
         AboutUsComponent,
         ContactUsComponent,
-        HotelComponent,
-        SearchHotelPipe,
-        EventsAdminComponent,
-        HotelViewComponent,
-        SearchHotelTablePipe,
-        SearchEventPipe,
-        ServicesAdminComponent,
-        SearchServicePipe,
-        RoomAdminComponent,
-        TypeRoomAdminComponent,
-        SearchTypeRoomPipe,
-        SearchRoomPipe,
-        LayoutAdminHotelComponent,
-        SidebarAdminHotelComponent,
-        HomeAdminHotelComponent,
-        SidebarrightAdminHotelComponent,
-        HeaderAdminHotelComponent,
-        ProfileAdminHotelComponent,
-        ServicesAdminHotelComponent,
-        EventsAdminHotelComponent,
-        TypesRoomsAdminHotelComponent,
-        RoomsAdminHotelComponent,
-        SearchUserBarPipe,
-        SearchEventBarPipe,
-        SearchHotelBarPipe,
-        SearchRoomBarPipe,
-        SearchServiceBarPipe,
-        SearchTypeRoomBarPipe,
-        ViewRoomDetailsComponent,
-        SearchEventHotelPipe,
-        SearchServiceHotelPipe,
-        SearchRoomHotelPipe,
-        SearchTypeRoomHotelPipe,
-        SidebarUserComponent,
-        SidebarrightUserComponent,
-        LayoutUserComponent,
-        HeaderUserComponent,
-        HomeUserComponent,
-        HotelUserComponent,
-        HotelAdminHotelComponent,
-        UserProfileComponent,
-        UserCalendarComponent,
-        HistoryComponent,
-        SearchRoomViewPipe,
-        ReservationsReportComponentHotel,
-        ReservationsComponent,
         NavbarComponent,
         DevelopsComponent,
         SearchUserPipe
